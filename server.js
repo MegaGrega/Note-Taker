e76753f0bc37b514fb04ade4c68b0ff6aa370e47
MegaGrega <mkmgrega@gmail.com>
2020-07-14 23:14:45 -0700

    added correct server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -38,4 +38,8 @@ app.get("/notes", function (req, res) {
 // Displays all characters
 app.get("/api/notes", function (req, res) {
     return res.json(testData);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
