f10d092804d2ede7c3346132c01b19defaab3cb0
MegaGrega <mkmgrega@gmail.com>
2020-07-15 00:11:34 -0700

    fixed file structure

--- server.js
+++ server.js
@@ -12,39 +12,29 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-// JSON TEST DATA
-// =============================================================
-function testData(req,res){
-    fs.readFile("db.json", function (err, data) {
-        if (err) throw err;
-        // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-        // an html file.
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });
-}
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // Displays all characters
-app.get("/api/notes", function (req, res) {
-    return res.json(testData(req,res));
-});
+// app.get("/api/notes", function (req, res) {
+//     return 
+// });
 // Making new commit test
 
 
