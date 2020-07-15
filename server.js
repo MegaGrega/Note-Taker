01925cdba7a1e53134e6262e3d24e77ffc04e0f2
MegaGrega <mkmgrega@gmail.com>
2020-07-14 23:11:01 -0700

    basic server functionality, basic readfile

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// JSON TEST DATA
+// =============================================================
+var testData = fs.readFile("db.json", function (err, data) {
+    if (err) throw err;
+    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+    // an html file.
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+});
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// Displays all characters
+app.get("/api/notes", function (req, res) {
+    return res.json(testData);
+});
